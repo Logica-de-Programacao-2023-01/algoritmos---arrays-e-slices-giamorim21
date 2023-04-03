2cdf4d650cce9cbe6fb0d6c8d544a4d7fdb44cc3
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-03 20:13:53 -0300

    Update algoritmos4.go

--- algoritmos4.go
+++ algoritmos4.go
@@ -6,25 +6,26 @@ import "fmt"
 //e os valores dos elementos. Em seguida, imprima o Slice e a soma dos valores armazenados.
 
 func main() {
-	// Solicita ao usuário o tamanho do Slice
+	
 	var x int
-	fmt.Print("Digite o tamanho do Slice: ")
+	fmt.Println("Digite o tamanho do Slice: ")
 	fmt.Scan(&x)
 
-	// Cria o Slice com o tamanho informado pelo usuário
+	
 	slice := make([]int, x)
 
-	// Solicita ao usuário os valores dos elementos do Slice
+
 	for i := 0; i < x; i++ {
-		fmt.Printf("Digite o valor do elemento %d: ", i+1)
+		fmt.Println("Digite o valor do elemento" , i + 1 , ": ")
 		fmt.Scan(&slice[i])
 	}
 
-	// Imprime o Slice e a soma dos valores armazenados
-	fmt.Printf("Slice: %v\n", slice)
-	sum := 0
+	
+	fmt.Println("Slice: ", slice)
+	soma := 0
 	for _, value := range slice {
-		sum += value
+		soma += value
 	}
-	fmt.Printf("Soma dos valores: %d\n", sum)
+	fmt.Println("Soma dos valores: ", soma)
+    
 }
