61886af6039b2fcadcadd8325a0ebbd4ec73ebc3
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 13:45:07 -0300

    Update exercicio5.go

--- exercicio5.go
+++ exercicio5.go
@@ -2,20 +2,27 @@ package main
 
 import "fmt"
 
-//Crie uma matriz bidimensional de inteiros com 3 linhas e 4 colunas.
-//Inicialize cada elemento com o valor da soma do índice da linha e
+//Crie uma matriz bidimensional de inteiros com 3 linhas
+//e 4 colunas.
+//Inicialize cada elemento com o valor da soma do índice
+//da linha e
 //o índice da coluna.
 //Imprima a matriz resultante.
 
 func main() {
-	var matriz [3][4]int
-
-	for linha := range matriz {
-		for coluna, elemento := range matriz[linha] {
-			elemento = linha + coluna
-			fmt.Println("Elemento[", linha, "][", coluna, "]=", elemento)
+	// cria a matriz de inteiros
+	matriz := [3][4]int{}
+	
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 4; j++ {
+			matriz[i][j] = i + j
 		}
-
 	}
 
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 4; j++ {
+			fmt.Print(matriz[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
