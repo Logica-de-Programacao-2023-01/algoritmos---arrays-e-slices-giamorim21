7857b00f2b86967935b9a9ec68e4ea0ae6af1111
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 12:53:30 -0300

    Update algoritmos13.go

--- algoritmos13.go
+++ algoritmos13.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+//Crie um Array de inteiros com 7 elementos.
+//Solicite ao usuário que informe um número que será
+//adicionado ao primeiro e ao último elemento do Array.
+//Imprima o Array resultante.
+
+func main() {
+	array := [7]int{1, 2, 3, 4, 5, 6, 7}
+
+	var numero int
+	fmt.Println("Digite um número para adicionar ao primeiro e ao último elemento do array:")
+	fmt.Scan(&numero)
+
+	array[0] += numero
+	array[6] += numero
+
+	fmt.Println(array)
+}
+
