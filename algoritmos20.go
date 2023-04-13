ab344f7a7eb5967b3b993586bcf1e703df42b0f7
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 13:33:08 -0300

    Update algoritmos20.go

--- algoritmos20.go
+++ algoritmos20.go
@@ -1,21 +1,24 @@
 package main
 
+//Crie um programa que leia um array de inteiros
+//e verifique se ele está ordenado em ordem crescente.
+
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
 	crescente := true
-	for i := 1; i < len(slice); i++ {
-		atual := slice[i]
-		anterior := slice[i-1]
-
-		if anterior >= atual {
+	for i := 1; i < len(array); i++ {
+		if array[i] < array[i-1] {
 			crescente = false
 			break
 		}
 	}
+
 	if crescente {
-		fmt.Println()
+		fmt.Println("O array está em ordem crescente!")
+	} else {
+		fmt.Println("O array não está em ordem crescente!")
 	}
 }
