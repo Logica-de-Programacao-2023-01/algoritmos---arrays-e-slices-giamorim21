516d2ac4b66b9a4c6bd78084991b0b20030de035
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 12:38:41 -0300

    Update algoritmos11.go

--- algoritmos11.go
+++ algoritmos11.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+//Crie um Array bidimensional de inteiros com 2 linhas
+//e 3 colunas. Em seguida, solicite ao usuário que informe
+//um índice de linha e outro de coluna
+//e imprima o valor armazenado nessa posição da matriz.
+
+func main() {
+	var matriz = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+
+	var linha, coluna int
+	fmt.Println("Digite um índice de linha (0 ou 1):")
+	fmt.Scan(&linha)
+	fmt.Println("Digite um índice de coluna (0, 1 ou 2):")
+	fmt.Scan(&coluna)
+
+	fmt.Printf("O valor armazenado na posição (%d, %d) é %d\n", linha, coluna, matriz[linha][coluna])
+
+}
+
