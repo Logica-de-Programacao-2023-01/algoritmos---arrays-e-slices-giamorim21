cba1b50bee20d7f2c73821c1f854215b99c956ee
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 12:43:39 -0300

    Update algoritmos10.go

--- algoritmos10.go
+++ algoritmos10.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+//Crie um Slice de inteiros com tamanho 10
+//e imprima o valor mínimo
+//e o valor máximo armazenados no Slice.
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	min := slice[0]
+	for _, valor := range slice {
+		if valor < min {
+			min = valor
+		}
+	}
+	max := slice[0]
+	for _, valor := range slice {
+		if valor > max {
+			max = valor
+		}
+	}
+	fmt.Printf("Valor mínimo: %d\n", min)
+	fmt.Printf("Valor máximo: %d\n", max)
+}
