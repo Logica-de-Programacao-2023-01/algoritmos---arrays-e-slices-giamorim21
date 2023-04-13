63a16099b39dabcad66386a8c71d5851a4d255ac
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-12 22:13:36 -0300

    Update algoritmos6.go

--- algoritmos6.go
+++ algoritmos6.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+//Crie um Array de inteiros com 10 elementos.
+//Em seguida, solicite ao usuário que informe um valor
+//e verifique se esse valor está presente no Array.
+//Imprima o resultado da busca.
+
+func main() {
+	var numeros = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var valor int
+	var achou bool
+	fmt.Println("Digite um valor:")
+	fmt.Scan(&valor)
+	for _, num := range numeros {
+		if valor == num {
+			achou = true
+			break
+		}
+	}
+	if achou {
+		fmt.Println("O valor digitado foi encontrado")
+	} else {
+		fmt.Println("Não foi encontrado")
+	}
+
+}
