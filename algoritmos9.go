162f82ae79a0e1d2851db6fa5ef4f11c34137023
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 10:47:05 -0300

    Update algoritmos9.go

--- algoritmos9.go
+++ algoritmos9.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+//Crie um Array de floats com 6 elementos.
+//Solicite ao usuário que informe um número que será
+//adicionado em todas as posições do Array.
+//Imprima o Array resultante.
+
+func main() {
+	var n [6]float64
+	var valor float64
+	fmt.Println("Informe um número a ser adicionado ao array:")
+	fmt.Scan(&valor)
+
+	for i := 0; i < len(n); i++ {
+		n[i] = valor
+	}
+	fmt.Println(n)
+}
